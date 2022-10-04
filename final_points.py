4765b3eeb6c56b263ff5584412d39146d4c9708b
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-10-03 17:21:44 -0700

    bug fixes

--- final_points.py
+++ final_points.py
@@ -11,8 +11,8 @@ class final_points(final):
         self._goal = goal / total_points
     
     def calculate_need_grd(self):
-        self._need_grade = (self._goal - self._current_grade) / self._final_worth * 100
-        self._need_grade_point = self._goal - self._current_grade
+        self._need_grade = (self._goal_point - self._current_grade) / self._final_worth * 100
+        self._need_grade_point = self._goal_point - self._current_grade
 
     def need_grd_print(self):
         print(f"\nYou need {self._need_grade_point} points (or {self._need_grade} percent) on the final to get a {self._goal_point} point (or {self._goal} percent) overall grade.")
