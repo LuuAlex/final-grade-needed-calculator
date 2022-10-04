094dc08a30d2d4629337e324c2f7aa8b31a66784
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-10-03 17:11:26 -0700

    override need_grade_print

--- final_points.py
+++ final_points.py
@@ -8,10 +8,14 @@ class final_points(final):
         self._total_points = total_points
         self._final_worth = final_worth
         self._goal_point = goal
-        self._goal_percentage = goal / total_points
+        self._goal = goal / total_points
     
     def calculate_need_grd(self):
         self._need_grade = (self._goal - self._current_grade) / self._final_worth * 100
+        self._need_grade_point = self._goal - self._current_grade
+
+    def need_grd_print(self):
+        print(f"\nYou need {self._need_grade_point} points (or {self._need_grade} percent) on the final to get a {self._goal_point} point (or {self._goal} percent) overall grade.")
 
 
 
