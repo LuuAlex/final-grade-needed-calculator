6cbec52d551a9a9b5b1f91f2300d19877ee73396
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-10-25 23:45:32 -0700

    progress on website

--- static/home.js
+++ static/home.js
@@ -1,1 +1,12 @@
 console.log("run")
+
+function get_inputs() {
+    return {
+        grade: document.getElementById('grade').value
+    }
+}
+
+$.post( "/postmethod", {
+    javascript_data: get_inputs()
+});
+
--- views.py
+++ views.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, render_template, jsonify
+import json
+from flask import Blueprint, render_template, request
 
 views = Blueprint(__name__, "view")
 
 
 @views.route("/")
 def home():
-    return render_template("home.html")
+    result = get_post_javascript_data()  # menu.getResult()
+    return render_template("home.html", result=result)
 
 
+@views.route('/postmethod', methods=['POST'])
+def get_post_javascript_data():
+    jsdata = request.form['javascript_data']
+    return json.loads(jsdata)[0]
+
+
+"""
 @views.route("/json")
 def get_json():
     return jsonify({'id': 1})
+"""
