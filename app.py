dbdecb1429fe8accd721fb117866f592417e2637
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-10-26 20:02:58 -0700

    save

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ from flask import Flask, render_template, request, jsonify
 # from views import views
 
 app = Flask(__name__)
+
+
 # app.register_blueprint(views, url_prefix="/")
 
 
 @app.route("/")
 def home():
-    #result = get_post_javascript_data()  # menu.getResult()
+    # result = get_post_javascript_data()  # menu.getResult()
     return render_template("home.html", result="HI")
 
 
 @app.route('/get_data', methods=['GET', 'POST'])
 def get_data():
     if request.method == 'GET':
-        message = {'greeting':'Hello from Flask!'}
+        message = {'greeting': 'Hello from Flask!'}
         return jsonify(message)
     if request.method == 'POST':
         print(request.get_json())
         return 'Sucesss', 200
-
-
 
 
 if __name__ == '__main__':
